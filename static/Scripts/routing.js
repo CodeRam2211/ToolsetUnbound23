0d45a8fbbd9937147157b7b519625b9120668022
ShenLong058031 <49099980+CodeRam2211@users.noreply.github.com>
2023-06-09 02:17:43 +0530

    Added Flask App

--- static/Scripts/routing.js
+++ static/Scripts/routing.js
@@ -2,20 +2,20 @@
 
 
 
-if(location.href == "http://127.0.0.1:5500/Pages/index.html"){
+if(location.href == "http://127.0.0.1:5000/"){
     const login = document.querySelector(".in-btn")
     login.addEventListener("click", ()=>{
-        location.replace("/Pages/dashboard.html")
+        location.replace("/dashboard")
     })
 }
 
-if(location.href == "http://127.0.0.1:5500/Pages/dashboard.html"){
+if(location.href == "http://127.0.0.1:5000/dashboard"){
     const signout = document.querySelector(".lout")
     const logo = document.querySelector(".logo")
     const myFiles = document.querySelector(".my-files")
     signout.addEventListener("click", ()=>{
         console.log("Clicked")
-        location.replace("/Pages/index.html")
+        location.replace("/")
     })
     logo.addEventListener("click", ()=>{
         console.log("Clicked")
