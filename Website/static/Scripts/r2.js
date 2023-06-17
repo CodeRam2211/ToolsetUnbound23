11842e786b90f0e188137555e14d565a8c9e74b2
mannm2406 <mannm2406@gmail.com>
2023-06-17 21:47:21 +0530

    More design changes(unfinished)

--- Website/static/Scripts/r2.js
+++ Website/static/Scripts/r2.js
@@ -9,7 +9,7 @@ if(location.href == "http://127.0.0.1:5000/"){
     })
 }
 
-if(location.href != "http://127.0.0.1:5000/"){
+if(location.href != "http://127.0.0.1:5000"){
     const signout = document.querySelector(".lout")
     const logo = document.querySelector(".logo")
     const myFiles = document.querySelector("#files")
